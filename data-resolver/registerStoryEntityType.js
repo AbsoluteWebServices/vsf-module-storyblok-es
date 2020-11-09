303a19d01fa115c79124981902b656844b521550
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2020-11-10 01:45:14 +0200

    feat(config): add editable elasticsearch host

--- data-resolver/registerStoryEntityType.js
+++ data-resolver/registerStoryEntityType.js
@@ -1,4 +1,15 @@
+import config from 'config'
+import { processURLAddress } from '@vue-storefront/core/helpers'
+import getApiEndpointUrl from '@vue-storefront/core/helpers/getApiEndpointUrl'
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
+
 export default function registerStoryEntityType (searchAdapter) {
+  let url = config.storyblok.elasticsearchHost
+
+  if (!url) {
+    url = processURLAddress(getApiEndpointUrl(currentStoreView().elasticsearch, 'host'))
+  }
+
   searchAdapter.registerEntityType('story', {
     queryProcessor: (query) => {
       // function that can modify the query each time before it's being executed
@@ -6,6 +17,7 @@ export default function registerStoryEntityType (searchAdapter) {
     },
     resultProcessor: (resp, start, size) => {
       return searchAdapter.handleResult(resp, 'story', start, size)
-    }
+    },
+    url
   })
 }
